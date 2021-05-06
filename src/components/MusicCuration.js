49405be9345e191d5ac7ba85a9fd5f3e12d212e1
Sara Ward <saraward@MacBook-Pro-6.local>
2021-05-06 00:10:01 -0400

    music curation

---
+++ src/components/MusicCuration.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const MusicCuration = () => {
+    return (
+        <div>
+            <p>hello music curation page</p>
+        </div>
+    )
+}
+
+export default MusicCuration
