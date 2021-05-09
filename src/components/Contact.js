46bb19feb330eb8255fea02f9174c0b5e8c27743
Gourav <gore.auluck@gmail.com>
2021-05-08 22:42:09 -0700

    Mobile Contact / Updated Footer

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,18 +1,51 @@
-import React from 'react'
-import "../styles/Contact.css"
-
+import React from "react";
+import "../styles/Contact.css";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-    return (
-        <div className="contactdiv">
-            <p>Reach out and let us know how we can help you & your event get on the Good Foot
-            Our goal is to facilitate success for performers, venues, festivals and events 
-            through constantly evolving creative management. We have worked with top quality talent, 
-            festivals and venues both locally and nationally. We relish every opportunity to make events 
-            worthwhile, create memorable experiences, and take great pride in our reputation for doing so 
-            with an upbeat positive energy.</p>
-        </div>
-    )
-}
+  function sendEmail(e) {
+    e.preventDefault();
 
-export default Contact
+    emailjs
+      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_USER_ID")
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+    alert("Thank your for connecting! I'll be in touch soon!");
+  }
+
+  return (
+    <div className="contactContainer">
+      <h1>GET ON THE GOOD FOOT</h1>
+      <div className="flexContainer">
+        <p>
+          Reach out and let us know how we can help you & your event get on the
+          Good Foot. Our goal is to facilitate success for performers, venues,
+          festivals and events through constantly evolving creative management.
+          We have worked with top quality talent, festivals and venues both
+          locally and nationally. We relish every opportunity to make events
+          worthwhile, create memorable experiences, and take great pride in our
+          reputation for doing so with an upbeat positive energy.
+        </p>
+        <form className="contact-form" onSubmit={sendEmail}>
+          {/* <input type="hidden" name="contact_number" /> */}
+          <label>Name</label>
+          <input type="text" name="name" placeholder="Name" required />
+          <label>Email</label>
+          <input type="email" name="email" placeholder="Email" required />
+          <label>Message</label>
+          <textarea name="message" placeholder="Drop me a line..." required />
+          <input id="button" type="submit" value="Send" />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,24 @@
-import React from 'react'
-import {Link} from 'react-router-dom' 
-import "../styles/Footer.css"
+import React from "react";
+import "../styles/Footer.css";
 
 const Footer = () => {
-    return (
-        <div className='footer'>
-   <p>im the footer</p>
-        </div>
-    )
-}
+  return (
+    <div className="footer">
+      <i class="fab fa-facebook-square"></i>
+      <a
+        href="https://www.gocalavera.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="https://res.cloudinary.com/dvug8hy5k/image/upload/v1620533450/Portfolio1/Calavera_oxwdwu.png"
+          alt="Logo"
+          className="logo"
+        />
+      </a>
+      <i class="fab fa-instagram-square"></i>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
